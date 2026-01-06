0bf7355719ad8951ec142b28ab158ac875c332fa
Jaehyeok Choi <jaehyeok@iastate.edu>
2026-01-06 17:49:39 -0600

    intermediate

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Following are the citations of the paper that are additionally used
 
 - Amparore, Elvio Gilberto, et al. "i: A Variable Order Metric for DEDS Subject to Linear Invariants." International Conference on Tools and Algorithms for the Construction and Analysis of Systems. Cham: Springer International Publishing, 2019.
 
+- Cayir, Sinan, and Müjgân Uçer. "An algorithm to compute a basis of Petri net invariants." 4th ELECO Int. Conf. on Electrical and Electronics Engineering. UCTEA, Bursa, Turkey. 2005.
+
 The PNML files are from MCC 2025
 - https://mcc.lip6.fr/2025/
 
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import numpy as np
+from scipy.linalg import null_space
+
 import math
+import sys
+import xml.etree.ElementTree as ET
+import os
+import pprint
 
 def leading(row):
     for i,v in enumerate(row):
@@ -124,7 +130,150 @@ def example():
     tab(rrff(A))
     print("------------------------------")
 
+# takes an incident matrix (A), |P| (m) and |T| (n)
+def algorithm1(A, m, n):
+    AT = A.T
+    I = np.identity(n, dtype=int)
+    k = 1
+    l = 1
+    print(A)
+    for i in range(min(n,m)):
+        pivot_row = np.argmax(np.abs(A[i:n,i])) + i
+
+        if A[pivot_row, i] == 0:
+            continue
+        
+        A[[i, pivot_row]] = A[[pivot_row, i]]
+        I[[i, pivot_row]] = I[[pivot_row, i]]
+        
+        for j in range(i + 1, n):
+            factor = A[j, i] / A[i, i]
+            A[j, i:] -= factor * A[i, i:]
+            I[j, :] -= factor * I[i, :]             
+    print("-----------")
+    print(A)
+    print("-----------")
+    print(I)
+    print("-----------")   
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <xml_file>")
+        return
+    
+    filename = sys.argv[1]
+    root = "/Users/jaehyeokchoi/Desktop/cesr-safe-pnml/"
+
+    # Parse PNML file
+    try:
+        tree = ET.parse(root + filename)
+        root = tree.getroot()
+
+        outfile_name = os.path.basename(filename)
+
+        ns_uri = root.tag.split("}")[0].strip("{")
+
+        # PLACES
+        places_nodes = root.findall(f".//{{{ns_uri}}}place")        
+        places_dic = {}
+        initial_markings = []
+
+        num_places = len(places_nodes)
+
+        for i in range(0, len(places_nodes)):
+            places_dic[places_nodes[i].get("id")] = i + 1
+            
+            initial_marking_node = places_nodes[i].find(f"{{{ns_uri}}}initialMarking/{{{ns_uri}}}text")
+            initial_marking = initial_marking_node.text if initial_marking_node is not None else None
+            if initial_marking is not None:
+                initial_markings.append(i+1)
+
+        # Transitions
+        transitions_nodes = root.findall(f".//{{{ns_uri}}}transition")
+        transitions_dic = {}
+
+        num_transitions = len(transitions_nodes)
+
+        for i in range(0, len(transitions_nodes)):
+            transitions_dic[transitions_nodes[i].get("id")] = i + 1 
+
+        # ARCS
+        arcs_nodes = root.findall(f".//{{{ns_uri}}}arc")
+        # this should have transition as key and input output specified
+        input_arc = {}
+        output_arc = {}
+        Cm = np.zeros((num_places, num_transitions), dtype=int)
+        Cp = np.zeros((num_places, num_transitions), dtype=int)
+        for i in range(0, len(arcs_nodes)):
+            source = arcs_nodes[i].get("source")
+            target = arcs_nodes[i].get("target")
+            # This is input arc
+            if source in places_dic:
+                Cm[places_dic[source]-1, transitions_dic[target]-1] -= 1
+                if target not in input_arc:
+                    input_arc[target] = [source]
+                else:
+                    input_arc[target].append(source)
+  
+            # this is output arc
+            elif target in places_dic:
+                Cp[places_dic[target]-1, transitions_dic[source]-1] += 1
+                if source not in output_arc:
+                    output_arc[source] = [target]
+                else:
+                    output_arc[source].append(target)
+
+        C = Cp + Cm
+
+        # This is for output file BRAVE_DD we don't care about this yet
+        # file_path = f"{outfile_name}.txt"
+
+        # with open(file_path, "w") as f:
+        #     f.write("BDD\n")
+        #     f.write(f"numvars: {len(places_nodes)}\n")
+        #     f.write(f"initialMarking:\n")
+        #     for i in initial_markings:
+        #         f.write(f"\t{i}\n")
+        #     f.write("BDD-DONE\n")
+        #     f.write("RELATIONS\n")
+        #     for i in transitions_dic:
+        #         if (i not in input_arc) and (i not in output_arc):
+        #             print("ERROR")
+        #             print(i + " is not in any")
+        #             break
+
+        #         f.write("RELATION\n")
+        #         f.write("INPUTS\n")
+        #         input_open = True
+        #         if i in input_arc:
+        #             for j in input_arc[i]:
+        #                 f.write(f"\t{places_dic[j]}\n")
+        #             f.write("INPUTS-DONE\n")
+        #             input_open = False
+        #         if (input_open):
+        #             f.write("INPUTS-DONE\n")
+
+
+        #         output_open = True
+        #         f.write("OUTPUTS\n")
+        #         if i in output_arc:
+        #             for j in output_arc[i]:
+        #                 f.write(f"\t{places_dic[j]}\n")
+        #             f.write("OUTPUTS-DONE\n")
+        #             output_open = False
+        #         if (output_open):
+        #             f.write("OUTPUTS-DONE\n")
+
+        #         f.write("RELATION-DONE\n")
+
+        #     f.write("RELATIONS-DONE\n")
+    
+    
+    except Exception as e:
+        print("Error reading XML:", e)
+        return
+
 if __name__ == "__main__":
-    example()
+    main()
     
     
